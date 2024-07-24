8d9f12e75b88b4a6800f49524b90359858ddc7c7
elizauh <yeurenah@unl.edu.ec>
2024-07-23 23:20:42 -0500

    Correción de ejecución

--- app.py
+++ app.py
@@ -5,12 +5,30 @@ import io
 import sys
 import subprocess
 
-
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/analyze', methods=['POST'])
+def analyze():
+    code = request.form['code']
+    tokens = lexical_analysis(code)
+    parse_tree = syntactic_analysis(tokens)
+    semantic_errors = semantic_analysis(tokens)
+    word_count, words = count_words_in_tokens(tokens)
+    reserved_words = find_reserved_words(tokens)
+    tree_json = graph_to_json(parse_tree)
+    
+    return jsonify({
+        'tokens': tokens,
+        'parse_tree': tree_json,
+        'semantic_errors': semantic_errors,
+        'word_count': word_count,
+        'words': words,
+        'reserved_words': reserved_words
+    })
 
 @app.route('/execute', methods=['POST'])
 def execute():
@@ -44,7 +62,6 @@ def execute():
         'execution_output': execution_output,
         'execution_error': execution_error
     })
-
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
